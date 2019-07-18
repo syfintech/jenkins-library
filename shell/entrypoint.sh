5e4cb45905da3150def99a927e89b3492a50199e
Jetzhu <512704820@qq.com>
2019-07-18 09:39:33 +0800

    Update entrypoint.sh

--- shell/entrypoint.sh
+++ shell/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-cd /${DEPLOY_PATH}/${MODULE}
+cd ${DEPLOY_PATH}/${MODULE}
 java -Djava.security.egd=file:/dev/./urandom -Dloader.path=.,../lib ${JAVA_OPTS} -jar ${JAR_FILE} --spring.profiles.active=${SPRING_PROFILE}
